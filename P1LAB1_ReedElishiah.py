81608267590f02ca3cac7d1882c39042ab920506
Elishiah13 <reede2244@student.faytechcc.edu>
2025-09-22 12:46:07 -0400

    First commit: add P1LAB1_ReedElishiah.py

---
+++ P1LAB1_ReedElishiah.py
@@ -0,0 +1,7 @@
+#Elishiah Reed
+#9/20/2025
+#P1LAB1
+#Creating a program for CTI110 using Python
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name")
+print("Hello,", first_name, last_name + "! Welcome to CTI 110")
