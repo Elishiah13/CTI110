972082f74739aeb94316047542656243127afbf2
Elishiah13 <reede2244@student.faytechcc.edu>
2025-10-19 09:40:31 -0400

    Add files via upload

---
+++ P3LAB_ReedElishiah.py
@@ -0,0 +1,53 @@
+# Elishiah Reed
+#P3Lab3
+#10/19/2025
+# Program to help find the proper amount of pennies, dimes, nickels, dollars and quarters for an amount of money. 
+
+amount = float(input("Enter the amount of money: "))
+
+cents = int(round(amount * 100))
+
+dollars = cents // 100
+cents = cents % 100
+
+quarters = cents // 25
+cents = cents % 25
+
+dimes = cents // 10
+cents = cents % 10
+
+nickels = cents // 5
+cents = cents % 5
+
+pennies = cents
+
+
+if dollars > 0:
+    if dollars == 1:
+        print("1 dollar")
+    else:
+        print(f"{dollars} dollars")
+
+if quarters > 0:
+    if quarters == 1:
+        print("1 quarter")
+    else:
+        print(f"{quarters} quarters")
+
+if dimes > 0:
+    if dimes == 1:
+        print("1 dime")
+    else:
+        print(f"{dimes} dimes")
+
+if nickels > 0:
+    if nickels == 1:
+        print("1 nickel")
+    else:
+        print(f"{nickels} nickels")
+
+if pennies > 0:
+    if pennies == 1:
+        print("1 penny")
+    else:
+        print(f"{pennies} pennies")
